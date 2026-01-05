8f9c118f70d11f1d42f04f79a389fdb7ba5311f5
lfygit1 <827241059@qq.com>
2026-01-05 17:50:33 +0800

    inderict 参数用法

---
+++ 7-pytest单元测试框架模块/test_demo1/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+@pytest.fixture()
+def inderict_fixture(request):
+    user = request.param
+    print(f"传入的用户名：{user}")
+    yield user
+
+@pytest.fixture()
+def inderict_fixture(request):
+    userinfo = request.param
+    print(f"传入的用户名：{userinfo['user']}")
+    print(f"传入的密码：{userinfo['pwd']}")
+    yield userinfo
+
+
+@pytest.fixture()
+def inderict_fixture1(request):
+    user = request.param
+    print(f"传入的用户名：{user}")
+
+@pytest.fixture()
+def inderict_fixture2(request):
+    pwd = request.param
+    print(f"传入的密码：{pwd}")
---
+++ 7-pytest单元测试框架模块/test_demo1/test_demo11.py
@@ -0,0 +1,25 @@
+import pytest
+
+# 1. 单fixture  单值 的传参
+# user = ['张三','李四']
+# @pytest.mark.parametrize('inderict_fixture',user,indirect=True)
+# def test_case01(inderict_fixture):
+#     print(f'测试方法的用户名：{inderict_fixture}')
+
+
+
+
+# 2. 单fixture  多值 的传参
+# userinfo = [{'user':'张三','pwd':'123456'},{'user':'李四','pwd':'654321'}]
+# @pytest.mark.parametrize('inderict_fixture',userinfo,indirect=True)
+# def test_case01(inderict_fixture):
+#     print(f'测试方法的用户名：{inderict_fixture['user']}')
+#     print(f'测试方法的用户名：{inderict_fixture['pwd']}')
+
+
+# 3. 多fixture  多值 的传参（通过嵌套元组的列表）
+usermsg = [('张三','123456'),('李四','654321')]
+@pytest.mark.parametrize('inderict_fixture1,inderict_fixture2',usermsg,indirect=True)
+def test_case01(inderict_fixture1,inderict_fixture2):
+    print('这是测试用例')
+
